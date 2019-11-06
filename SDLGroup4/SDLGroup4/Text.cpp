42b28df15e2c03632413b7f6453884744861f094
Mandy_Fu <man.chunfu@futuregames.nu>
2019-11-06 13:30:40 +0100

    text.cpp

--- SDLGroup4/SDLGroup4/Text.cpp
+++ SDLGroup4/SDLGroup4/Text.cpp
@@ -4,15 +4,29 @@
 #include <SDL_ttf.h>
 #include <iostream>
 
-
-SDL_Texture* Engine::Text::ChooseFont(const std::string& font_path, int fontsize)
+Engine::Text::Text(SDL_Renderer* renderer, const std::string& font_path, int fontsize, const std::string& message_text, SDL_Color color)
 {
-	auto font = TTF_OpenFont(font_path.c_str(), fontsize);
+	textTexture = LoadFont(Engine::Window::Renderer, font_path, fontsize, message_text, color);
+	SDL_QueryTexture(textTexture, nullptr, nullptr, &textRect.w, &textRect.h);
+}
+
+
+SDL_Texture* Engine::Text::LoadFont(SDL_Renderer* renderer, const std::string& font_path, int fontsize, const std::string& message_text, SDL_Color color)
+{
+	TTF_Font* font = TTF_OpenFont(font_path.c_str(), fontsize);
 	if (!font)
-		std::cout << "Failed to load font. SDL_Error: " << SDL_GetError << std::endl;
-	else
-	{
-		
-	}
-	return nullptr;
+		std::cout << "Failed to load font. SDL Error: " << SDL_GetError << std::endl;
+
+	SDL_Surface* textSurface = TTF_RenderText_Solid(font, message_text.c_str(), color);
+	if (!textSurface)
+		std::cout << "Failed to create text_surface." << SDL_GetError << std::endl;
+
+	SDL_Texture* textTexture = SDL_CreateTextureFromSurface(Engine::Window::Renderer, textSurface);
+	if (!textTexture)
+		std::cout << "Failed to create text_texture." << SDL_GetError << std::endl;
+
+	SDL_FreeSurface(textSurface);
+	TTF_CloseFont(font);
+
+	return textTexture;
 }
--- SDLGroup4/SDLGroup4/Text.h
+++ SDLGroup4/SDLGroup4/Text.h
@@ -2,20 +2,22 @@
 
 #include <string>
 #include <SDL_rect.h>
+
 struct SDL_Texture;
 struct SDL_Color;
+struct SDL_Renderer;
 
 namespace Engine
 {
 	class Text
 	{
 	public:
-
+		Text(SDL_Renderer* renderer, const std::string& font_path, int fontsize, const std::string& message_text, SDL_Color color);
 	private:
-		SDL_Texture* ChooseFont(const std::string& font_path, int fontsize);
+		SDL_Texture* LoadFont(SDL_Renderer* renderer, const std::string& font_path, int fontsize, const std::string& message_text, SDL_Color color);
 
-		SDL_Texture* fontTexture;
-		//SDL_Rect textRect;
+		SDL_Texture* textTexture;
+		SDL_Rect textRect;
 	};
 }
 
