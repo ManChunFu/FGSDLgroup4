bb46ca477d52e3bd7be21172e9b5040186d70672
Mandy_Fu <man.chunfu@futuregames.nu>
2019-11-29 10:12:16 +0100

    score board updated

--- SDLGroup4/Game/ScoreBoard.cpp
+++ SDLGroup4/Game/ScoreBoard.cpp
@@ -24,27 +24,28 @@ void ScoreBoard::SetUpUI()
 	ThisScene = this;
 	topTenScoreListTitle = new Engine::Text("Assets/Fonts/BAUHS93.ttf", 50, "TOP 10 SCORES", { 0, 150, 200, 255 }, { 50, 50, 550, 100 });
 	
-	backToMenuButton = new Engine::Button({ 350, 80, 530, 800 }, { 0, 255, 0, 255 });
-	backToMenuText = new Engine::Text("Assets/Fonts/BAUHS93.ttf", 45, "BACK TO MENU", { 255, 255, 255, 255 }, { 45, 45, 40, 15 });
+	backToMenuButton = new Engine::Button({ 350, 80, 550, 750 }, { 0, 255, 0, 255 });
+	backToMenuText = new Engine::Text("Assets/Fonts/BAUHS93.ttf", 45, "BACK TO MENU", { 255, 255, 255, 255 }, { 45, 45, 25, 15 });
 	backToMenuButton->SetOnClickEvent(OnClickMenuButton);
 	
 	std::vector<Engine::GameElement*> scoreTexts;
 	Engine::UIManager::AddObjectsToCanvas(4, { topTenScoreListTitle, backToMenuButton});
 
 	float textPosY = 200;
-	//std::vector<ScoreRecord*> scores = scoreRecorder->GetSavedRecords();
+	int index = 1;
 	for (auto score : scoreRecorder->GetSavedRecords())
 	{
 		if (textPosY == 200)
 		{
-			scoreTexts.push_back(new Engine::Text("Assets/Fonts/Roboto-Medium.ttf", 50,
-				score->Date + " | " + std::to_string(score->Score), { 255, 255, 255, 255 }, { 50, 50, 100, textPosY }));
+			scoreTexts.push_back(new Engine::Text("Assets/Fonts/Roboto-Medium.ttf", 30,
+				std::to_string(index) + "     " + score->Date + "  |   " + std::to_string(score->Score), { 255, 255, 255, 255 }, { 50, 50, 500, textPosY }));
 		}
 		else
-			scoreTexts.push_back(new Engine::Text("Assets/Fonts/Roboto-Medium.ttf", 50,
-				score->Date + " | " + std::to_string(score->Score), { 255, 255, 255, 255 }, { 50, 50, 100, textPosY}));
+			scoreTexts.push_back(new Engine::Text("Assets/Fonts/Roboto-Medium.ttf", 30,
+				std::to_string(index) + "     " + score->Date + "  |   " + std::to_string(score->Score), { 255, 255, 255, 255 }, { 50, 50, 500, textPosY}));
 
-		textPosY += 100;
+		textPosY += 50;
+		index++;
 	}
 	Engine::UIManager::AddObjectsToCanvas(4, scoreTexts);
 
@@ -60,7 +61,6 @@ void ScoreBoard::Start()
 
 void OnClickMenuButton()
 {
-
 	ScoreBoard::ThisScene->LoadScene(0);
 }
 
